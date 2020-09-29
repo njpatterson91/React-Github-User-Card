fd091237e10cd0d9b9537ce9ad6e588717ca5059
Nathaniel Patterson <odst0016@gmail.com>
2020-09-29 15:31:47 -0400

    getting data from api and placing in state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React from "react";
 import axios from "axios";
 import Search from "./components/Search";
+import Results from "./components/Results";
 
 class App extends React.Component {
   state = {
     userDetails: [],
-    userNames: [],
+    userName: "odst0016",
   };
 
+  getData = (userName) => {
+    axios.get(`https://api.github.com/users/${userName}`).then((res) => {
+      this.setState({ userDetails: res.data });
+    });
+  };
+
+  componentDidMount() {
+    this.getData(this.state.userName);
+  }
+
   render() {
+    console.log(this.state.userDetails);
     return (
       <div>
         <h1>Github User Search</h1>
         <Search />
+        <Results />
       </div>
     );
   }
