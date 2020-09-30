b5a898c66a35e5b0733a50305e10c195fe0c4067
Nathaniel Patterson <odst0016@gmail.com>
2020-09-29 20:51:19 -0400

    mvp met

--- src/App.js
+++ src/App.js
@@ -2,9 +2,19 @@ import React from "react";
 import axios from "axios";
 import Search from "./components/Search";
 import Results from "./components/Results";
+import styled from "styled-components";
+
+const MainFlex = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  background-color: grey;
+  text-align: center;
+`;
 
 class App extends React.Component {
   state = {
+    followerDetails: [],
     userDetails: [],
     userName: "odst0016",
   };
@@ -13,6 +23,11 @@ class App extends React.Component {
     axios.get(`https://api.github.com/users/${userName}`).then((res) => {
       this.setState({ userDetails: res.data });
     });
+    axios
+      .get(`https://api.github.com/users/${userName}/followers`)
+      .then((res) => {
+        this.setState({ followerDetails: res.data });
+      });
   };
 
   componentDidMount() {
@@ -27,17 +42,27 @@ class App extends React.Component {
     e.preventDefault();
     this.getData(this.state.userName);
   };
+  handleClick = (e) => {
+    // this.setState({
+    //   userName: e.target.id,
+    // });
+    this.getData(e.target.id);
+  };
 
   render() {
     return (
-      <div>
+      <MainFlex>
         <h1>Github User Search</h1>
         <Search
           handleSearch={this.handleSearch}
           handleUserChange={this.handleUserChange}
         />
-        <Results data={this.state.userDetails} />
-      </div>
+        <Results
+          data={this.state.userDetails}
+          followerData={this.state.followerDetails}
+          handleClick={this.handleClick}
+        />
+      </MainFlex>
     );
   }
 }
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,4 +1,13 @@
 import React from "react";
+import Followers from "./Followers";
+import styled from "styled-components";
+
+const FlexxyBoi = styled.div`
+  display: flex;
+  max-width: 100%;
+  flex-flow: row wrap;
+  justify-content: space-evenly;
+`;
 
 class Results extends React.Component {
   render() {
@@ -12,6 +21,18 @@ class Results extends React.Component {
           Has {userData.followers} followers and is following{" "}
           {userData.following}
         </p>
+        <hr />
+        <h2>Followers</h2>
+        <hr />
+        <FlexxyBoi>
+          {this.props.followerData.map((item) => (
+            <Followers
+              data={item}
+              key={item.login}
+              handleClick={this.props.handleClick}
+            />
+          ))}
+        </FlexxyBoi>
       </div>
     );
   }
